32a519ab25d2980c3ed9ff61f91da223496b298f
Elton Zheng <elton.zheng.sh@gmail.com>
2022-06-10 00:11:50 +0800

    completed this demo project.

---
+++ src/main/java/ink/pwr/springboot2demo/restservice/HelloController.java
@@ -0,0 +1,12 @@
+package ink.pwr.springboot2demo.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @GetMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+}
